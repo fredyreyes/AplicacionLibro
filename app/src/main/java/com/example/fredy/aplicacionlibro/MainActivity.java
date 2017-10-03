2c6d3ab25db7fc0ab8b28f89f300aeb53c6c8fcc
koanco@hotmail.com <koanco@hotmail.com>
2017-10-02 23:11:42 -0500

    commit-11-11pm

--- app/src/main/java/com/example/fredy/aplicacionlibro/MainActivity.java
+++ app/src/main/java/com/example/fredy/aplicacionlibro/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 //Johann Polania Gonzalez
     //SI FUNCIONO
+    //otro cambio
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
