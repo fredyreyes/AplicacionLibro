d0972a3c86b568b604991237d9e1692f8072694c
koanco@hotmail.com <koanco@hotmail.com>
2017-10-02 22:13:43 -0500

    commit-johann

--- app/src/main/java/com/example/fredy/aplicacionlibro/MainActivity.java
+++ app/src/main/java/com/example/fredy/aplicacionlibro/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 //TE AMO JOHAN!!
 public class MainActivity extends AppCompatActivity {
 //Johann Polania Gonzalez
+    //SI FUNCIONO
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
