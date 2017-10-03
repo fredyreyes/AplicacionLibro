7664b8b5883d8892c7baed6cf639254f56b783af
koanco@hotmail.com <koanco@hotmail.com>
2017-10-02 22:59:16 -0500

    cambio johann 11 pm

--- app/src/main/java/com/example/fredy/aplicacionlibro/MainActivity.java
+++ app/src/main/java/com/example/fredy/aplicacionlibro/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.fredy.aplicacionlibro;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 //TE AMO JOHAN!!
+//JOHANN POLANA GONZALEZ
 public class MainActivity extends AppCompatActivity {
 
     @Override
