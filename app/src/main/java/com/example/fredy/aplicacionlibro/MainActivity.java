e06a276ef45315eedc8f794051048946ca49ebf0
koanco@hotmail.com <koanco@hotmail.com>
2017-10-07 10:48:31 -0500

    cambios en mostrar resources

--- app/src/main/java/com/example/fredy/aplicacionlibro/MainActivity.java
+++ app/src/main/java/com/example/fredy/aplicacionlibro/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.fredy.aplicacionlibro;
 
+import android.app.Application;
 import android.content.Context;
+import android.content.res.Resources;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -31,14 +33,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Instantiate a ViewPager
 
+        Resources res = getResources();
+        String[] autores = res.getStringArray(R.array.Autores);
+        String[]  textoAutores = res.getStringArray(R.array.textoAutores);
+
 
        this.pager = (ViewPager) this.findViewById(R.id.pager);
-      buttonShare=(Button)findViewById(R.id.buttonShare);
+        buttonShare=(Button)findViewById(R.id.buttonShare);
+
+
+
 
         // Create an adapter with the fragments we show on the ViewPager
      MyFragmentPagerAdapter adapter = new MyFragmentPagerAdapter(
                 getSupportFragmentManager());
-       adapter.addFragment(Autor1Fragment.newInstance("EDUARDO GALEANO","VENAS ABIERTAS","IMAGEN_1",1));
+       adapter.addFragment(Autor1Fragment.newInstance(autores[0],textoAutores[0],"IMAGEN_1",1));
        adapter.addFragment(Autor1Fragment.newInstance("JULIO CORTAZAR","RAYUELA","IMAGEN_1",2));
         adapter.addFragment(Autor1Fragment.newInstance("GABRIEL GARCÍA MARQUEZ","CIEN AÑOS DE SOLEDAD","IMAGEN_1",3));
         adapter.addFragment(Autor1Fragment.newInstance("JOSE ASUNCIÓN SILVA","De sobremesa","IMAGEN3",4));
