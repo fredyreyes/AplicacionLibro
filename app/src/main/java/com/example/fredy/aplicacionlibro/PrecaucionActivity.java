e44288f81f732bac7aa6de2a1a7baf076e49bf63
koanco@hotmail.com <koanco@hotmail.com>
2017-10-21 19:05:23 -0500

    lastVersion21OctubrePrecaucion

--- app/src/main/java/com/example/fredy/aplicacionlibro/PrecaucionActivity.java
+++ app/src/main/java/com/example/fredy/aplicacionlibro/PrecaucionActivity.java
@@ -1,18 +1,22 @@
 package com.example.fredy.aplicacionlibro;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class PrecaucionActivity extends AppCompatActivity {
 
     ViewPager pager = null;
-    TypedArray imgs2;
+    TypedArray imgs;
     MyFragmentPagerAdapter pagerAdapter;
     @SuppressWarnings("ResourceType")
     @Override
@@ -21,21 +25,31 @@ public class PrecaucionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_precaucion);
 
         Resources res = getResources();
-        String[] listaImagenesAutores = res.getStringArray(R.array.ImagesTextos);
 
-        this.pager = (ViewPager) this.findViewById(R.id.pager);
+        imgs = getResources().obtainTypedArray(R.array.precaucionImagenes);
 
+
+        this.pager = (ViewPager) this.findViewById(R.id.pagerPrecaucion);
+
+
+
+        // Create an adapter with the fragments we show on the ViewPager
         MyFragmentPagerAdapter adapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
 
-        for(int i=0;i<imgs2.length()-1;i++)
+        for(int i=0;i<imgs.length();i++)
         {
-            int urlImagen=imgs2.getResourceId(i,-1);
+            int urlImagen=imgs.getResourceId(i,-1);
 
             adapter.addFragment(Autor1Fragment.newInstance(String.valueOf(urlImagen),i));
 
 
         }
+
+
+
         this.pager.setAdapter(adapter);
+
+
     }
 
     public void ShowCurrentItem(View v){
