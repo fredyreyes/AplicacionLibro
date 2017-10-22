b1206f8726b36eac4dc5576278b0162fe16d8b2f
koanco@hotmail.com <koanco@hotmail.com>
2017-10-21 20:08:52 -0500

    lastVersion21OctubrePrecaucion

--- app/src/main/java/com/example/fredy/aplicacionlibro/MenuActivity.java
+++ app/src/main/java/com/example/fredy/aplicacionlibro/MenuActivity.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -26,7 +27,91 @@ Button bFavoritos;
         bFavoritos=(Button) findViewById(R.id.bFavoritos);
         bTextos=(Button) findViewById(R.id.bTextos);
 
+        bPrecaucion.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    bPrecaucion.setBackgroundResource(R.drawable.precaucion_enabled);
+                }
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    bPrecaucion.setBackgroundResource(R.drawable.precaucion);
+                }
+                return false;
+            }
+        });
+
+
+        bAutores.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    bAutores.setBackgroundResource(R.drawable.autores_enabled);
+                }
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    bAutores.setBackgroundResource(R.drawable.autores);
+                }
+                return false;
+            }
+        });
+
+
+
+        bCreditos.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    bCreditos.setBackgroundResource(R.drawable.creditos_enabled);
+                }
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    bCreditos.setBackgroundResource(R.drawable.creditos);
+                }
+                return false;
+            }
+        });
+
+
+        bTextos.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    bTextos.setBackgroundResource(R.drawable.texto_enabled);
+                }
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    bTextos.setBackgroundResource(R.drawable.texto);
+                }
+                return false;
+            }
+        });
+
+
+
+
+        bFavoritos.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    bFavoritos.setBackgroundResource(R.drawable.favoritos_enabled);
+                }
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    bFavoritos.setBackgroundResource(R.drawable.favoritos);
+                }
+                return false;
+            }
+        });
+
+
+
+
     }
+
+
+
+
 
 
     public void ShowActivity(View view){
@@ -35,23 +120,23 @@ Button bFavoritos;
         switch (view.getId())
         {
             case R.id.bPrecaucion:
-                bPrecaucion.setBackgroundResource(R.drawable.precaucion_enabled);
+
 
                 i = new Intent(this, PrecaucionActivity.class );
                 startActivity(i);
-                bPrecaucion.setBackgroundResource(R.drawable.precaucion);
+
                 break;
 
             case R.id.bCreditos:
-                bCreditos.setBackgroundResource(R.drawable.creditos_enabled);
+
                 i = new Intent(this, CreditosActivity.class );
                 startActivity(i);
-                bCreditos.setBackgroundResource(R.drawable.creditos);
+
                 break;
 
             case R.id.bFavoritos:
 
-                bFavoritos.setBackgroundResource(R.drawable.favoritos_enabled);
+
                 Favorites objDatabase =  new Favorites(this, "DbAplicacionLibro", null, 1);
                 SQLiteDatabase dbFavorites = objDatabase.getWritableDatabase();
                 Cursor c= dbFavorites.rawQuery("SELECT * FROM Favoritos",null);
@@ -69,15 +154,15 @@ Button bFavoritos;
 
 
                 }
-                bFavoritos.setBackgroundResource(R.drawable.favoritos);
+
                 break;
 
 
             case R.id.bTextos:
-                bTextos.setBackgroundResource(R.drawable.texto_enabled);
+
                 i = new Intent(this, MainActivity.class );
                 startActivity(i);
-                bTextos.setBackgroundResource(R.drawable.texto);
+
                 break;
             case R.id.bAutor:
                 i = new Intent(this, Autores.class );
